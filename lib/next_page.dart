ca03e0ed9af98dc29a77ceb0ba76f7cf3d3e3129
takagiri <takagiri.yuuki@rion.co.jp>
2021-11-05 14:11:58 +0900

    2

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -9,7 +9,7 @@ class NextPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Next Page takagiri"),
+        title: Text("Next Page takagiri2"),
       ),
     );
   }
