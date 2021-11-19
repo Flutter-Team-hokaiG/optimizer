e23521491b784a21624250c9cb5c84f11cfae528
takagiri <takagiri.yuuki@rion.co.jp>
2021-11-19 15:31:36 +0900

    スマートコントロールのようなボリューム作成

--- lib/takagiri_page.dart
+++ lib/takagiri_page.dart
@@ -44,34 +44,68 @@ int _value = 0;
 bool _enableDragging = true;
 
 class _TakagiriSliderState extends State<TakagiriSlider> {
+  void _valueup() {
+    if (_value < 5) {
+      setState(() {
+        _value++;
+      });
+    } else {
+      return null;
+    }
+  }
+
+  void _valuedown() {
+    if (_value > -5) {
+      setState(() {
+        _value--;
+      });
+    } else {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      child: SfSlider.vertical(
-        min: min_value,
-        max: max_value,
-        value: _value,
-        stepSize: 1,
-        interval: 1,
-        activeColor: Colors.blue,
-        inactiveColor: Colors.blue.withOpacity(0.3),
-        showTicks: true,
-        showLabels: true,
-        enableTooltip: true,
-        showDividers: true,
-        minorTicksPerInterval: 1,
-        onChanged: (dynamic newvalue) {
-          if (_enableDragging == true) {
-            setState(() {
-              _value = newvalue;
-            });
-          } else {
-            return null;
-          }
-        },
-      ),
-    );
+        //height: 300,
+        child: Column(
+      children: [
+        Container(
+          height: 200,
+        ),
+        Text(
+          "$_value",
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.blue,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: _valueup,
+          child: Icon(Icons.add),
+        ),
+        SfSlider.vertical(
+          min: min_value,
+          max: max_value,
+          value: _value,
+          //stepSize: 1,
+          interval: 1,
+          activeColor: Colors.blue,
+          inactiveColor: Colors.blue.withOpacity(0.3),
+          //showTicks: true,
+          //showLabels: true,
+          //enableTooltip: true,
+          //showDividers: true,
+          onChanged: (dynamic value) {
+            setState(() {});
+          },
+        ),
+        ElevatedButton(
+          onPressed: _valuedown,
+          child: Icon(Icons.remove),
+        ),
+      ],
+    ));
   }
 }
 
@@ -99,7 +133,6 @@ class _TakagiriSlider2State extends State<TakagiriSlider2> {
         showLabels: true,
         enableTooltip: true,
         showDividers: true,
-        minorTicksPerInterval: 1,
         onChanged: (dynamic newvalue) {
           if (_enableDragging == true) {
             setState(() {
